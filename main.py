6cb6b3669abf22d83190eddba976ee4b38fafac8
mikey788 <97480161+mikey788@users.noreply.github.com>
2024-01-11 00:56:20 +0000

    Wed, Jan 10, 2024, 7:56 PM -05:00

--- main.py
+++ main.py
@@ -40,5 +40,5 @@ def video_feed():
     return Response(generate_frame(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
